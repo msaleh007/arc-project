0b00679c9844f625974b94e7949a1ddee951d2bb
msaleh007 <adeelzafar419@gmail.com>
2026-05-23 01:56:06 +0500

    feat: Deploy GiwaContract on GIWA Sepolia Testnet - 2 contracts deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,11 @@ require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.24",
-  networks: {
+  networks: {giwa_testnet: {
+      url: "https://rpc.sepolia.giwa.io",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 91342
+    },
     arc_testnet: {
       url: "https://rpc.testnet.arc.network",
       chainId: 5042002,
